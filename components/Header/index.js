2141e8c38d07323437d6071acfbe9d30cc15d794
Christopher Pupo <chrispupo@Christophers-iMac.local>
2019-07-12 16:08:13 -0400

    working header

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,10 +3,47 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
+const lTimesDate = 'MARCH 28, 2019';
+const lTimesTitle = 'Lambda Times';
+const lTimesTemp = '98°';
 
-function Header() {}
+const headerSet = document.querySelector('.header-container')
+//console.log(headerSet)
+
+headerSet.appendChild(header())
+
+function header(headerInfo) {
+    //creating elements
+    const mainHeader = document.createElement('div')
+
+    const date = document.createElement('span')
+    
+
+    const title = document.createElement('h1')
+  
+    const temp = document.createElement('span')
+    
+
+    //setting up the structure for the elements
+    mainHeader.appendChild(date)
+    mainHeader.appendChild(title)
+    mainHeader.appendChild(temp)
+
+    //setting the styles up
+    mainHeader.classList.add('header')
+    date.classList.add('date')
+    title.classList.add('h1')
+    temp.classList.add('temp')
+
+    //setting the content for the elements
+    date.textContent = lTimesDate
+    title.textContent = lTimesTitle
+    temp.textContent = lTimesTemp
+
+    return mainHeader
+}
